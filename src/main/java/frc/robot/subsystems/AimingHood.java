383ba9c46508f8aaab35e7146b30069c4bdcc900
Maris Usis <marisusis@inbox.lv>
2020-02-27 16:42:11 -0500

    added basic IO for hood

--- src/main/java/frc/robot/subsystems/AimingHood.java
+++ src/main/java/frc/robot/subsystems/AimingHood.java
@@ -1,4 +1,55 @@
 package frc.robot.subsystems;
 
-public class AimingHood {
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.ControlType;
+import com.revrobotics.EncoderType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.lib.can.BetterSpark;
+import frc.lib.scheduling.Scheduler;
+import frc.lib.subsystem.Subsystem;
+import frc.robot.Constants;
+
+public final class AimingHood extends Subsystem {
+
+    private final IO io = new IO();
+    private final BetterSpark hoodControl = new BetterSpark(Constants.HOOD_CH, MotorType.kBrushed);
+    private final CANEncoder encoder = new CANEncoder(hoodControl, EncoderType.kQuadrature, 8192);
+
+    public AimingHood() {
+        super("Shooter Hood");
+    }
+
+    @Override
+    public void check() {
+        encoder.setPositionConversionFactor(1.0);
+    }
+
+    @Override
+    public void readIO() {
+        io.position = encoder.getPosition();
+    }
+
+    @Override
+    public void writeIO() {
+        hoodControl.set(ControlType.kDutyCycle, io.output);
+    }
+
+    @Override
+    public void updateDashboard() {
+        SmartDashboard.putNumber("hood.position", io.position);
+        SmartDashboard.putNumber("hood.output", io.output);
+    }
+
+    @Override
+    public void registerLoops(Scheduler scheduler) {
+
+    }
+
+    private static final class IO {
+        private double output, position;
+
+        private IO() {}
+    }
 }
