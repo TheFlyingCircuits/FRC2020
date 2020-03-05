6fd3de0874a02d10edc203a3c97564ba4500a5ab
Maris Usis <marisusis@inbox.lv>
2020-03-05 11:02:58 -0500

    button class implementation start

--- src/main/java/frc/lib/command/trigger/Button.java
+++ src/main/java/frc/lib/command/trigger/Button.java
@@ -1,4 +1,60 @@
 package frc.lib.command.trigger;
 
-public class Button {
+import frc.lib.command.Command;
+import frc.lib.subsystem.CommandSubsystem;
+import frc.lib.subsystem.Subsystem;
+
+import java.util.function.BooleanSupplier;
+
+public class Button extends Trigger {
+    public Button() {}
+
+    public Button(BooleanSupplier isPressed) {
+        super(isPressed);
+    }
+
+    public Button whenPressed(final Command command, boolean interruptible) {
+        whenActive(command, interruptible);
+        return this;
+    }
+
+    public Button whenPressed(final Command command) {
+        whenActive(command);
+        return this;
+    }
+
+    public Button whenPressed(final Runnable toRun, Class<? extends CommandSubsystem>... requirements) {
+        whenActive(toRun, requirements);
+        return this;
+    }
+
+    public Button whileHeld(final Command command, boolean interruptible) {
+        whileActiveContinuous(command, interruptible);
+        return this;
+    }
+
+    public Button whileHeld(final Command command) {
+        whileActiveContinuous(command);
+        return this;
+    }
+
+    public Button whileHeld(final Runnable toRun, Class<? extends CommandSubsystem>... requirements) {
+        whileActiveContinuous(toRun, requirements);
+        return this;
+    }
+
+    public Button whenReleased(final Command command, boolean interruptible) {
+        whenInactive(command, interruptible);
+        return this;
+    }
+
+    public Button whenReleased(final Command command) {
+        whenInactive(command);
+        return this;
+    }
+
+    public Button whileReleased(final Runnable toRun, Class<? extends CommandSubsystem>... requirements) {
+        whenInactive(toRun, requirements);
+        return this;
+    }
 }
